86035409309beb2be355daa57dfde0ab4b6d1818
Youngjoo Park <yjoo9317@gmail.com>
2018-04-16 22:31:58 -0700

    Linking program and associating vertex data with attribute(p.50)

--- app/src/main/java/com/example/yjoo9/airhockey/AirHockeyRenderer.java
+++ app/src/main/java/com/example/yjoo9/airhockey/AirHockeyRenderer.java
@@ -3,6 +3,7 @@ package com.example.yjoo9.airhockey;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 
+import com.example.yjoo9.airhockey.android.util.LoggerConfig;
 import com.example.yjoo9.airhockey.android.util.ShaderHelper;
 import com.example.yjoo9.firstopengl.R;
 
@@ -13,15 +14,26 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
+    private static final String U_COLOR = "u_Color";
+    private static final String A_POSITION = "a_Position";
     private static final int POSITION_COMPONENT_COUNT=2;
     private static final int BYTES_PER_FLOAT = 4;
     private final FloatBuffer mVertexData;
     private final Context mContext;
+
+    private int mProgram;
+    private int mUColorLocation;
+    private int mAPosition;
 
     public AirHockeyRenderer(Context context){
         this.mContext = context;
@@ -54,6 +66,19 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
         int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
         int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+
+        mProgram = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+
+        if(LoggerConfig.ON){
+            ShaderHelper.validateProgram(mProgram);
+        }
+        glUseProgram(mProgram);
+
+        mUColorLocation = glGetUniformLocation(mProgram, U_COLOR);
+        mAPosition = glGetAttribLocation(mProgram, A_POSITION);
+
+        mVertexData.position(0);
+        glVertexAttribPointer(mAPosition, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, mVertexData);
     }
 
     @Override
--- app/src/main/java/com/example/yjoo9/airhockey/android/util/ShaderHelper.java
+++ app/src/main/java/com/example/yjoo9/airhockey/android/util/ShaderHelper.java
@@ -4,14 +4,22 @@ import android.util.Log;
 
 import static android.opengl.GLES20.GL_COMPILE_STATUS;
 import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
+import static android.opengl.GLES20.GL_LINK_STATUS;
+import static android.opengl.GLES20.GL_VALIDATE_STATUS;
 import static android.opengl.GLES20.GL_VERTEX_SHADER;
+import static android.opengl.GLES20.glAttachShader;
 import static android.opengl.GLES20.glCompileShader;
 import static android.opengl.GLES20.glCreateProgram;
 import static android.opengl.GLES20.glCreateShader;
+import static android.opengl.GLES20.glDeleteProgram;
 import static android.opengl.GLES20.glDeleteShader;
+import static android.opengl.GLES20.glGetProgramInfoLog;
+import static android.opengl.GLES20.glGetProgramiv;
 import static android.opengl.GLES20.glGetShaderInfoLog;
 import static android.opengl.GLES20.glGetShaderiv;
+import static android.opengl.GLES20.glLinkProgram;
 import static android.opengl.GLES20.glShaderSource;
+import static android.opengl.GLES20.glValidateProgram;
 
 public class ShaderHelper {
     public static final String TAG = "ShaderHelper";
@@ -62,6 +70,33 @@ public class ShaderHelper {
             }
             return 0;
         }
+        glAttachShader(programObjectId, vertexShaderId);
+        glAttachShader(programObjectId, fragmentShaderId);
+
+        glLinkProgram(programObjectId);
+        final int[] linkStatus = new int[1];
+        glGetProgramiv(programObjectId, GL_LINK_STATUS, linkStatus, 0);
+
+        if(LoggerConfig.ON){
+            Log.v(TAG, "Results of linking program:\n"+glGetProgramInfoLog(programObjectId));
+        }
+        if(linkStatus[0] == 0){
+            glDeleteProgram(programObjectId);
+            if(LoggerConfig.ON){
+                Log.w(TAG, "Linking of program failed.");
+            }
+            return 0;
+        }
+
         return programObjectId;
+    }
+
+    public static boolean validateProgram(int programObjectId){
+        glValidateProgram(programObjectId);
+
+        final int[] validateStatus = new int[1];
+        glGetProgramiv(programObjectId, GL_VALIDATE_STATUS, validateStatus, 0);
+        Log.v(TAG, "Results of validating program: "+validateStatus[0]+"\n Log: "+glGetProgramInfoLog(programObjectId));
+        return validateStatus[0] != 0;
     }
 }
