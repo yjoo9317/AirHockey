1b39a3fe7196bb2829115b5b8b8fc5354b26a52d
Youngjoo Park <yjoo9317@gmail.com>
2018-04-30 23:07:22 -0700

    Update constant to blend (P71)

--- app/src/main/java/com/example/yjoo9/airhockey/AirHockeyRenderer.java
+++ app/src/main/java/com/example/yjoo9/airhockey/AirHockeyRenderer.java
@@ -18,6 +18,7 @@ import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_LINES;
 import static android.opengl.GLES20.GL_POINTS;
 import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
@@ -30,34 +31,53 @@ import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
+    private static final String A_COLOR = "a_Color";
+    private static final int COLOR_COMPONENT_COUNT = 3;
     private static final String U_COLOR = "u_Color";
     private static final String A_POSITION = "a_Position";
     private static final int POSITION_COMPONENT_COUNT=2;
     private static final int BYTES_PER_FLOAT = 4;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT+COLOR_COMPONENT_COUNT)*BYTES_PER_FLOAT;
+
     private final FloatBuffer mVertexData;
     private final Context mContext;
 
     private int mProgram;
     private int mUColorLocation;
     private int mAPositionLocation;
+    private int mAColorPosition;
 
     public AirHockeyRenderer(Context context){
         this.mContext = context;
         float[] tableVerticesWithTriangles = {
-                -0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f, 0.5f,
+           //   X,  Y,  R,  G,  B   adding color to each vertex
+                0f,     0f,  1f,   1f,   1f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.5f, 0.7f, 0.7f, .07f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
 
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
+
+                0f, -0.25f, 0f, 0f, 1f,
+                0f, 0.25f, 1f, 0f, 0f
+        };
+        /*float[] tableVerticesWithTriangles = {
+                0f, 0f,
                 -0.5f, -0.5f,
                 0.5f, -0.5f,
                 0.5f, 0.5f,
+                -0.5f, 0.5f,
+                -0.5f, -0.5f,
 
                 -0.5f, 0f,
                 0.5f, 0f,
 
                 0f, -0.25f,
                 0f, 0.25f
-        };
+        };*/
         mVertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length*BYTES_PER_FLOAT)
                 .order(ByteOrder.nativeOrder())
                 .asFloatBuffer();
@@ -99,7 +119,7 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
         // draw white table
         glUniform4f(mUColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
 
         // drawing center red line
         glUniform4f(mUColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
